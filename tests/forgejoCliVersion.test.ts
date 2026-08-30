aae592e376fe510992ed34f5431d126f8920e2de
David Siewert <david1gruppenplan@gmail.com>
2026-08-30 11:59:38 +0200

    test(cli): align version assertion with package

--- tests/forgejoCliVersion.test.ts
+++ tests/forgejoCliVersion.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "bun:test"
 import { forgejoCliVersion } from "../src/index.js"
 
 test("exposes the package version", () => {
-  expect(forgejoCliVersion).toBe("0.2.0")
+  expect(forgejoCliVersion).toBe("0.3.0")
 })
